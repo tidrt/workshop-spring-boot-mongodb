a8ca337b9c3bfb915955726c0845205fb44ad0bf
Tiago <tiago@ufrgs.br>
2023-01-19 10:43:39 -0300

    adding comments to posts

--- src/main/java/com/tidrt/workshopmongo/config/TestConfig.java
+++ src/main/java/com/tidrt/workshopmongo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.tidrt.workshopmongo.domain.Post;
 import com.tidrt.workshopmongo.domain.User;
 import com.tidrt.workshopmongo.dto.AuthorDTO;
+import com.tidrt.workshopmongo.dto.CommentDTO;
 import com.tidrt.workshopmongo.repository.PostRepository;
 import com.tidrt.workshopmongo.repository.UserRepository;
 
@@ -41,9 +42,18 @@ public class TestConfig implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+		
+		
 	}
 }
--- src/main/java/com/tidrt/workshopmongo/domain/Post.java
+++ src/main/java/com/tidrt/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.tidrt.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.tidrt.workshopmongo.dto.AuthorDTO;
+import com.tidrt.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,6 +21,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body; 
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -70,6 +75,15 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -95,5 +109,4 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
-
 }
