1059eed18b57f240c4b912182ef029d8c99baad8
Tiago <tiago@ufrgs.br>
2023-01-18 23:27:16 -0300

    getting a post by id

---
+++ src/main/java/com/tidrt/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.tidrt.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tidrt.workshopmongo.domain.Post;
+import com.tidrt.workshopmongo.repository.PostRepository;
+import com.tidrt.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+}
