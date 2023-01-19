57125e58b9b0fdd8f8bf4e3a8fa7dffdc47b7519
Tiago <tiago@ufrgs.br>
2023-01-18 22:54:25 -0300

    referencing user posts

--- src/main/java/com/tidrt/workshopmongo/config/TestConfig.java
+++ src/main/java/com/tidrt/workshopmongo/config/TestConfig.java
@@ -43,5 +43,7 @@ public class TestConfig implements CommandLineRunner {
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
