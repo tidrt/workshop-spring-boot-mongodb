a30a698f8bcb917d3bc46ec7be2289830f4f4938
Tiago <tiago@ufrgs.br>
2023-01-18 23:20:17 -0300

    endpoint to return user posts

--- src/main/java/com/tidrt/workshopmongo/resources/UserResource.java
+++ src/main/java/com/tidrt/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.tidrt.workshopmongo.domain.Post;
 import com.tidrt.workshopmongo.domain.User;
 import com.tidrt.workshopmongo.dto.UserDTO;
 import com.tidrt.workshopmongo.services.UserService;
@@ -66,4 +67,11 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
  	}
+	
+	@RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		// to convert our user list to a user dto list 
+		return ResponseEntity.ok().body(obj.getPosts()); 
+	}
 }
