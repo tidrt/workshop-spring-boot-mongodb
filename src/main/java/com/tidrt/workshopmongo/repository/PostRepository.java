865a0b8a766ed0389aadb8d0f2e67d3abb41ac91
Tiago <tiago@ufrgs.br>
2023-01-20 15:33:29 -0300

    simple research with query methods

--- src/main/java/com/tidrt/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/tidrt/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.tidrt.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.tidrt.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	// query method containing
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/tidrt/workshopmongo/resources/PostResource.java
+++ src/main/java/com/tidrt/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.tidrt.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tidrt.workshopmongo.domain.Post;
+import com.tidrt.workshopmongo.resources.util.URL;
 import com.tidrt.workshopmongo.services.PostService;
 
 @RestController
@@ -22,5 +26,13 @@ public class PostResource {
 		Post obj = service.findById(id);
 		// to convert our user list to a user dto list 
 		return ResponseEntity.ok().body((obj)); 
+	}
+	
+	@RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		// to convert our user list to a user dto list 
+		return ResponseEntity.ok().body(list); 
 	}
 }
