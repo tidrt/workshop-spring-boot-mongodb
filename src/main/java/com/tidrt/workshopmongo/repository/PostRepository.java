5633f3873c581a6e5563aafd6e7c52909e2d8104
Tiago <tiago@ufrgs.br>
2023-01-21 00:51:08 -0300

    simple search with @query

--- src/main/java/com/tidrt/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/tidrt/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.tidrt.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tidrt.workshopmongo.domain.Post;
@@ -12,4 +13,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	
 	// query method containing
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
